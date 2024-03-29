643e60b17a30472f2eb53ed5cebbaeda6fccea52
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-03-28 19:55:00 -0500

    Create switch statement to return correct choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * (4 - 1) + 1);   
+    
+    switch(choice) {
+        case 1: 
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default:
+            return null;
+    }
 }
